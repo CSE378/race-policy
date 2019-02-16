a85e01576cf43c866736f8dcfdf25ee0569dd077
Sayan Sivakumaran <sivakusayan@gmail.com>
2019-02-16 10:29:50 -0500

    Wrote rotateAxis function

--- rotateAxis.m
+++ rotateAxis.m
@@ -7,3 +7,7 @@ function Prot = rotateAxis(v, theta, P)
 %   P: 3*1 vector for a point in 3D space
 % Outputs:
 %   Prot: a 3*1 vector for the point P after rotation
+I = eye(3);
+K = [0 -v(3) v(2); v(3) 0 -v(1); -v(2) v(1) 0];
+R = I + sin(theta)*K + (1-cos(theta))*K^2;
+Prot = R*P;
